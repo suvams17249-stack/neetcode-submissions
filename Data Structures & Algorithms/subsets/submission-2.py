91f6ef2c572c6f861d779fe3acd1c5236067e9eb
suvams17249-stack <suvams17249@gmail.com>
2026-06-26 02:55:50 +0530

    Add: subsets - submission-2

---
+++ Data Structures & Algorithms/subsets/submission-2.py
@@ -0,0 +1,20 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+
+        result = []
+
+        def helper(i,temp):
+            if i == len(nums):
+                result.append(temp.copy())
+                return
+            temp.append(nums[i])
+            helper(i+1,temp)
+
+            temp.pop()
+            helper(i+1,temp)
+        helper(0,[])
+        return result
+
+
+
+        
